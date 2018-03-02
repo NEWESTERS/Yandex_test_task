2c4f19910309517f32357a8baf303967a70ef9ed
NEWESTERS <ramiro.cruz@mail.ru>
2018-03-03 01:57:00 +0300

    Almost done

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-  var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+function polarToCartesian( centerX, centerY, radius, angleInDegrees ) {
+	var angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
 
-  return {
-    x: centerX + (radius * Math.cos(angleInRadians)),
-    y: centerY + (radius * Math.sin(angleInRadians))
-  };
+	return {
+    	x: centerX + (radius * Math.cos(angleInRadians)),
+    	y: centerY + (radius * Math.sin(angleInRadians))
+  	};
 }
 
-function describeArc(x, y, radius, startAngle, endAngle){
-
-    var start = polarToCartesian(x, y, radius, endAngle);
+function describeArc( x, y, radius, startAngle, endAngle ) {
+	var start = polarToCartesian(x, y, radius, endAngle);
     var end = polarToCartesian(x, y, radius, startAngle);
 
     var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
@@ -22,29 +21,28 @@ function describeArc(x, y, radius, startAngle, endAngle){
     return d;       
 }
 
-function animatedDraw ( val ) {
+function animatedDraw( val ) {
 	var start = Date.now(); // сохранить время начала
 
 	var timer = setInterval(function() {
-	  // вычислить сколько времени прошло с начала анимации
-	  var timePassed = Date.now() - start;
+	  	// вычислить сколько времени прошло с начала анимации
+		var timePassed = Date.now() - start;
 
-	  if (timePassed > val * 5) {
-	    clearInterval(timer); // конец через 2 секунды
-	    return;
-	  }
+	 	if (timePassed > val * 5) {
+	    	clearInterval(timer); // конец через 2 секунды
+	    	return;
+	  	}
 
-	  // рисует состояние анимации, соответствующее времени timePassed
-	  draw(timePassed);
+	  	// рисует состояние анимации, соответствующее времени timePassed
+	  	draw(timePassed);
 
 	}, 20);
 
 	// в то время как timePassed идёт от 0 до 2000
 	// left принимает значения от 0 до 400px
 	function draw( timePassed ) {
-	  document.getElementById('progress').style.width = timePassed / 5 + 'px';
-	  var arc = describeArc(100, 100, 50, 0, timePassed / 5);
-	  document.getElementById("arc1").setAttribute("d", arc);
+	  	var arc = describeArc(90, 90, 70, 0, timePassed / 5);
+	  	document.getElementById("progress").setAttribute("d", arc);
 	}
 }
 
@@ -56,17 +54,66 @@ function Progress ( link ) {
 		this.updateProgress();
 	};
 
+	this.setMod = function( property, value ){
+		element.setAttribute(property, value);
+	};
+
 	this.updateProgress = function ( ) {
 		var val = 360 * (this.value / 100)
-		//element.style.width = val + 'px';
 		animatedDraw(val);
-		//var arc = describeArc(100, 100, 100, 0, var);
-		//document.getElementById("arc1").setAttribute("d", arc);
+	};
+
+	this.hide = function ( is_hide ) {
+		if(is_hide) {
+			element.setAttribute('display', 'none')
+		} else {
+			element.setAttribute('display', '')
+		}
 	};
 }
 
-window.onload = function () {		
+window.onload = function ( ) {		
 	progress = new Progress('progress');
-	progress.setValue(75);	
+	progress.setValue(75);
+
+	var percent = document.getElementById('percent');
+	percent.onchange = function() {
+		progress.setValue(this.value);
+	};		
+
+	var animate_switch = document.getElementById('animate-switch');	
+	animate_switch.onclick = function() {
+		switchOnclick(this);
+		if(this.getAttribute('enabled') == 'true') {
+			progress.setMod('animated','yes');
+		}
+		else {
+		  	progress.setMod('animated','');
+		}
+    }; 
+
+    var hide_switch = document.getElementById('hide-switch');	
+	hide_switch.onclick = function() {
+		switchOnclick(this)
+		if(this.getAttribute('enabled') == 'true') {
+			progress.hide(true);
+		} else {
+		  	progress.hide(false);
+		}
+    };
+
+    var switcher = document.getElementById('switch');
+    switcher.onclick = function() {
+    	switchOnclick(this);
+    }
 }
+
+function switchOnclick (switcher) {
+	if (switcher.getAttribute('enabled') == 'false') {
+    		switcher.setAttribute('enabled', 'true')
+    	} else {
+    		switcher.setAttribute('enabled', 'false')
+    	}
+}
+
 
